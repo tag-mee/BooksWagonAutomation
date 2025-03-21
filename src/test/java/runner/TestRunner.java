1e44337cdd52ff6d2f94d06795e3c416c7855a14
Aditya Jha <109282841+tag-mee@users.noreply.github.com>
2025-03-21 09:04:59 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(features = "src/test/resources/Allcombined/C-ProductFeature",
+@CucumberOptions(features = "src/test/resources/Allcombined",
 glue = "stepdefinition",
 plugin = { "pretty","json:target/cucumber-reports-json/Cucumber.json" }
 )
